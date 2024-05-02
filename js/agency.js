11e5b59c919fb90e10b378dc24840830777696c5
Yuting Weng <yuting17@uchicago.edu>
2024-05-02 11:56:13 -0500

    update

--- js/agency.js
+++ js/agency.js
@@ -36,12 +36,3 @@ $('div.modal').on('show.bs.modal', function() {
 	}
 });
 
-
-$('.dropdown-toggle').click(function(e) {
-    e.preventDefault();
-    setTimeout($.proxy(function() {
-      if ('ontouchstart' in document.documentElement) {
-        $(this).siblings('.dropdown-backdrop').off().remove();
-      }
-    }, this), 0);
-  });
